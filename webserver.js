6e68627ee1d9dbdf3d3efae5c6e9a125141ba507
Muhammad Mustafa <mustafalada36@gmail.com>
2023-07-31 02:43:30 +0500

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,1 +1,16 @@
-const http = require(http);
+const http = require('http');
+http.createServer((request,response) =>{
+	if (request.url ==="/"){
+	console.log(request.url)
+	response.writeHead(200, {'content-type': "text/html"})
+	response.write("<h1> Pizza </h1>")
+	response.end()
+	}
+	else if (request.url === "/menu"){
+		console.log(request.url)
+		response.writeHead(200, {'content-type': "text/html"})
+		response.write("<h1> Creamy  </h1>")
+		response.end()
+		}
+}
+).listen(3000)
